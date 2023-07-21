f2732ed7fc248e5170456818aabcc18615a97f5b
grantchenhuarong <chenhuarong@gpdi.com>
2023-07-21 18:12:48 +0800

    转换C3数据成为Belle的多轮对话语料

--- data_uniform/c3_uniform.py
+++ data_uniform/c3_uniform.py
@@ -1,17 +1,60 @@
 import json
-file_name = r"D:\temp\data\c3\d-dev.json"
+from template import PROMPT_TEMPLATE, USER_QUESTION_TEMPLATE, ASSISTANT_ANSWER_TEMPLATE
+
+file_name1 = r"D:\temp\data\c3\d-dev.json"
+file_name2 = r"D:\temp\data\c3\d-train.json"
+file_name3 = r"D:\temp\data\c3\m-dev.json"
+file_name4 = r"D:\temp\data\c3\m-train.json"
 
 
 def uniform(file):
+    json_dialogs = []
     with open(file, "r", encoding="utf-8") as f:
         examples = json.load(f)
         print(len(examples))
         print(examples[0])
         for example in examples:
-            if len(example) != 3:
-                print("length of example not equal 3: -------------------", example)
-            if len(example[1]) > 1:
-                print("question numbers greater than 1: -----------------", example)
+            # if len(example) != 3:
+            #     print("length of example not equal 3: -------------------", example)
+            # if len(example[1]) > 1:
+            #     print("question numbers greater than 1: -----------------", example)
+            # 处理场景
+            context = "\n".join(example[0])
+
+            # 计算问答对数量
+            question_cnt = len(example[1])
+            # 循环处理问答
+            instruction, output = "", ""
+            for i in range(question_cnt):
+                # 获取问题
+                if i == 0:
+                    question = example[1][i]['question'] + "可选答案有-" + "，".join(example[1][0]['choice'])
+                    instruction = PROMPT_TEMPLATE.format(context=context, question=question)
+                else:
+                    question = USER_QUESTION_TEMPLATE.format(question=example[1][i]['question'] +
+                                     "-可选答案有：" + "，".join(example[1][i]['choice']))
+                    # 构造 instruction
+                    instruction += question
+
+                # 获取回答
+                answer = ASSISTANT_ANSWER_TEMPLATE.format(answer=example[1][i]['answer'])
+
+                # 构造 instruction
+                if (i < question_cnt - 1):
+                    instruction += answer
+                else:
+                    # 构造 output
+                    output = example[1][i]['answer']
+
+            json_dialogs.append({"instruction": instruction, "input": "", "output": output})
+            # print(instruction + output)
+
+    return json_dialogs
 
 if __name__ == "__main__":
-    uniform(file_name)
+    json_dialogs1 = uniform(file_name1)
+    json_dialogs2 = uniform(file_name2)
+    json_dialogs3 = uniform(file_name3)
+    json_dialogs4 = uniform(file_name4)
+    json_dialogs = json_dialogs1 + json_dialogs2 + json_dialogs3 + json_dialogs4
+    json.dump(json_dialogs, open("./json/json_dialogs_c3.json", 'w', encoding='UTF-8'), ensure_ascii=False, indent=2)
--- data_uniform/template.py
+++ data_uniform/template.py
@@ -1,8 +1,12 @@
-PROMPT_TEMPLATE = """
-Human:已知信息：
+PROMPT_TEMPLATE = """已知信息：
 {context} 
+从上述已知信息，选择内容来回答用户的问题，不能添加已知信息之外的内容。如果无法从中得到答案，请直接回答 “根据已知信息无法回答该问题” 或 “没有提供足够的相关信息”。 
+问题是：{question}"""
 
-从上述已知信息，选择内容来回答用户的问题，不能添加已知信息之外的内容。如果无法从中得到答案，请直接回答 “根据已知信息无法回答该问题” 或 “没有提供足够的相关信息”。 \r\n
- \r\n问题是：{question}
-"""
+USER_QUESTION_TEMPLATE = """
+Human:问题是：{question}"""
 
+ASSISTANT_ANSWER_TEMPLATE = """
+Assistant:{answer}"""
+
+NO_ANSWER = "根据已知信息无法回答该问题"
